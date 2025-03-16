c22a4e9adcf1920780c686a5d91007840f9a592f
Ned Palacios <npdoesmc@gmail.com>
2025-03-16 20:55:04 +0800

    fix: cn

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,6 +1,6 @@
-import { ClassNameValue, twMerge } from "tailwind-merge";
-import clsx from "clsx";
+import { twMerge } from "tailwind-merge";
+import clsx, { ClassValue } from "clsx";
 
-export function cn(...args: ClassNameValue[]) {
-  return clsx(twMerge(...args));
+export function cn(...args: ClassValue[]) {
+  return twMerge(clsx(...args));
 }
